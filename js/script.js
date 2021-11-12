63837bd093416def4b1c1b4e29526e26a804d554
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-12 14:19:59 +0100

    Mia logica base

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* - stampare in pagina un item per ogni elemento contenuto in un array
+- ogni item ha una “x” associata: cliccando su di essa, l’item viene rimosso dalla lista
+- predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+il vostro array nei data non avrà delle semplici stringhe, ma sarà un array di oggetti;
+gli oggetti avranno questo formato:
+{ text: 'Fare la spesa', done: true }, 
+{ text: 'Fare il bucato', done: false }
+etc. etc..
+quindi nell’elenco di output ciò che sara “done” sarà barrato, il resto invece con testo normale.
+Nella richiesta base mi limito al fatto che quel “done” nei task inseriti dall’utente sarà sempre false,
+gli unici true saranno alcuni nei dati di partenza. */ 
+
+
+// 1. creare istanza vue associata a un id "app"-----> creare div sull html.
+// 2. in data inserire il nostro array di oggetti
+// 3 collegare sull html il nostro text all interno del nostro array di oggetti a un Ul li 
+// 3.1 se done = true , il testo sarà sbarrato ----> aggiungo classe con text-decoration line-through;
+// 4 aggiungo al mio text nell html una entity code o un pulsante con "x" dove associerò al click un evento ( togliere quell oggetto dal nostro array di oggetti)
